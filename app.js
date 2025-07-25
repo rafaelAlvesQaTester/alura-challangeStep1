edb877ada49f40ee7c83b7635ffaf903f1e5b049
Rafael Alves <168144782+rafaelAlvesQaTester@users.noreply.github.com>
2025-07-24 23:13:30 -0300

    versão 01

---
+++ app.js
@@ -0,0 +1,26 @@
+// Desafio 1: Mostre um alerta com a mensagem de boas-vindas
+alert('Bem-vindo ao nosso site!');
+
+// Desafio 2 a 5: Criação de variáveis com valores fixos
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponível = 1000;
+
+// Desafio 6: Exiba um alerta com uma mensagem de erro
+alert("Erro! Preencha todos os campos");
+
+// Desafio 7: Criar uma variável com mensagem de erro e exibi-la
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+// Desafio 8: Solicitar nome do usuário com prompt
+nome = prompt("Qual seu nome?");
+
+// Desafio 9: Solicitar idade do usuário e armazenar como número
+idade = prompt("Informe sua idade");
+
+// Desafio 10: Verificar se a idade é suficiente para tirar habilitação
+if (idade >= 18) {
+    alert("Pode tirar a habilitação!");
+}
